c66b3849f8f5a2074273b2a88f9204ca6450c792
MatRavi <101987948+MatRavi@users.noreply.github.com>
2022-12-02 16:12:37 -0300

    Add files via upload

---
+++ receber.php
@@ -0,0 +1,28 @@
+<?php
+  $nome = $_POST['nome'];
+  $email = $_POST['email'];
+  $mensagem = $_POST['mensagem'];
+  $data_envio = date('d/m/Y');
+  $hora_envio = date('H:i:s');
+
+  $arquivo = "
+    <html>
+      <p><b>Nome: </b>$nome</p>
+      <p><b>E-mail: </b>$email</p>
+      <p><b>Mensagem: </b>$mensagem</p>
+      <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
+    </html>
+  ";
+  
+  $destino = "matheusravier663@gmail.com";
+  $assunto = "Contato pelo Site";
+
+  $headers  = "MIME-Version: 1.0\n";
+  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
+  $headers .= "From: $email";
+
+  //Enviar
+  mail($destino, $assunto, $arquivo, $headers);
+  
+  echo "<meta http-equiv='refresh' content='10;URL=../coment.html'>";
+?>
